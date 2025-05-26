7d590addddd648853fa523f93531f8deb56249e8
rianfernandesno <zrpapz12@gmail.com>
2025-05-26 15:20:22 -0300

    Database seeding

--- src/main/java/com/yui/workshopmongo/models/entities/User.java
+++ src/main/java/com/yui/workshopmongo/models/entities/User.java
@@ -51,6 +51,10 @@ public class User {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
