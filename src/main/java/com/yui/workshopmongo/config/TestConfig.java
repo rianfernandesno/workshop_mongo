a4c5de49117dca31617a2ed74bfcab239a9b0e1a
rianfernandesno <zrpapz12@gmail.com>
2025-05-26 14:37:43 -0300

    Inserting User

---
+++ src/main/java/com/yui/workshopmongo/config/TestConfig.java
@@ -0,0 +1,31 @@
+package com.yui.workshopmongo.config;
+
+import com.yui.workshopmongo.models.entities.User;
+import com.yui.workshopmongo.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    public void init(){
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Gray", "bob@gmail.com");
+
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
